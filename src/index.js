481aa9b68800579f7cd81bcc829859038b69c3cc
Roman Didukh <newrdlink@yandex.ru>
2020-11-04 17:41:11 +0300

    add structure media

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log("Hello world")
+//  ссылка на кнопку трека
+const buttonTrack = document.querySelector('.media__button-track');
+// ссылка на кнопку открытия треков
+const buttonOpenTracks = document.querySelector('.media__button-open');
+// ссылка на контейнер с треками
+const mediaContentContainer = document.querySelector('.media__content');
+// функция открытия контейнера треков
+const toggleOpenTracks = () => {
+  mediaContentContainer.classList.toggle('media__content_opened');
+  buttonTrack.classList.toggle('media__button-track_opened');
+}
+// слущатель для кнопки открытия треков
+buttonOpenTracks.addEventListener('click', toggleOpenTracks);
